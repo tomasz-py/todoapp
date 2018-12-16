42c7aa068eb83150e232d6c2138480405bce61da
Tomasz-py <tomasz.pyclik@gmail.com>
2018-12-16 12:20:09 +0100

    Change ToDoList view

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const ToDoItem = ({ toDo }) => {
-  return <div style={{ color: "white" }}>{toDo}</div>;
+  //return <div style={{ color: "white" }}>{toDo}</div>;
+  return (
+    <div className="item">
+      <div className="content">{toDo}</div>
+    </div>
+  );
 };
 
 export default ToDoItem;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -5,6 +5,10 @@ const ToDoList = ({ toDoes }) => {
   const renderedList = toDoes.map((toDo, index) => {
     return <ToDoItem key={index} toDo={toDo} />;
   });
-  return <div className="ui relaxed divided list">{renderedList}</div>;
+  return (
+    <div className="ui inverted segment">
+      <div className="ui inverted relaxed divided list">{renderedList}</div>
+    </div>
+  );
 };
 export default ToDoList;
